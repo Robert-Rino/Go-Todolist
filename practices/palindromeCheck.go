f7f3e814aca40fbba1d0026b5d5adabab9a8f443
Nino.Swag <nino.chang@swag.live>
2025-07-19 21:40:08 +0800

    Add palindromeCheck.go

---
+++ practices/palindromeCheck.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isPalindrome(s string) bool {
+	left := 0
+	right := len(s) - 1
+
+	for {
+		if left > right {
+			return true
+		}
+
+		if s[left] != s[right] {
+			return false
+		}
+
+		left += 1
+		right -= 1
+	}
+
+}
+
+func main() {
+	fmt.Println("Waiting for input..., press Ctrl+D to exit")
+	stdin := bufio.NewScanner(os.Stdin)
+	for stdin.Scan() {
+		line := stdin.Text()
+		fmt.Println(isPalindrome(line))
+	}
+}
